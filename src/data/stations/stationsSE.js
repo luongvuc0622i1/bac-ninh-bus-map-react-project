edd87bd94db279a38b10a74478d4484c2298b318
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2023-07-25 07:13:03 +0700

    create file stationsSE.js

---
+++ src/data/stations/stationsSE.js
@@ -0,0 +1,200 @@
+export let stationsSE = {
+  'type': 'FeatureCollection',
+  'features': [
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.061027, 21.180761]
+      },
+      'properties': {
+        'id': 1,
+        'name': 'Bắc Ninh',
+        'description': 'Bến xe Bắc Ninh',
+        'address': '2 Nguyễn Du',
+        'district': 'Thành phố Bắc Ninh',
+        'routers': 'BN01, BN02, BN03, BN08, BN86A, BN86B'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.195603, 21.019009]
+      },
+      'properties': {
+        'id': 2,
+        'name': 'Lương Tài',
+        'description': 'Bến xe Lương Tài',
+        'address': 'Đường Phá Lãng',
+        'ward': 'Thị trấn Thứa',
+        'district': 'Lương Tài',
+        'routers': 'BN01'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.285208, 21.117838]
+      },
+      'properties': {
+        'id': 3,
+        'name': 'Phả Lại',
+        'description': 'Gần chân cầu Phả Lại',
+        'address': 'Quốc lộ 18',
+        'ward': 'Đức Long',
+        'district': 'Thị xã Quế Võ',
+        'routers': 'BN02'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.290945,21.040881]
+      },
+      'properties': {
+        'id': 4,
+        'name': 'Kênh Vàng',
+        'description': 'Dốc Kênh Vàng',
+        'address': 'Đường tỉnh 281',
+        'ward': 'Trung Kênh',
+        'district': 'Lương Tài',
+        'routers': 'BN08, BN27'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.013119, 21.030249]
+      },
+      'properties': {
+        'name': 'Xuân Lâm',
+        'description': 'Gần cổng KĐT Khai Sơn',
+        'address': 'Quốc lộ 17',
+        'ward': 'Xuân Lâm',
+        'district': 'Thị xã Thuận Thành',
+        'routers': 'BN27'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [105.967206,21.087379]
+      },
+      'properties': {
+        'name': 'Khu công nghiệp VSIP',
+        'description': 'Gần trung tâm văn hóa thể thao Khu phố Rích Gạo',
+        'address': '',
+        'ward': 'Phù Chẩn',
+        'district': 'Thành phố Từ Sơn',
+        'routers': 'BN68'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.143433,21.098960]
+      },
+      'properties': {
+        'name': '(B) Chi Lăng',
+        'description': 'Thôn Quế Ổ',
+        'address': '',
+        'ward': 'Chi Lăng',
+        'district': 'Thị xã Quế Võ',
+        'routers': 'BN68'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.265589, 20.989173]
+      },
+      'properties': {
+        'name': 'Minh Tân',
+        'description': 'Gần chợ Văn Thai',
+        'address': 'Đê Hữu Thái Bình',
+        'ward': 'Minh Tân',
+        'district': 'Lương Tài',
+        'routers': 'BN86A'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.201397, 21.086935]
+      },
+      'properties': {
+        'name': 'Đại Lai',
+        'description': 'Khu Di tích Lệ Chi Viên',
+        'address': 'Đường tỉnh 285',
+        'ward': 'Đại Lai',
+        'district': 'Gia Bình',
+        'routers': 'BN86B'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [105.849453,21.041309]
+      },
+      'properties': {
+        'name': 'Long Biên',
+        'description': 'Điểm trung chuyển Long Biên',
+        'address': '',
+        'ward': '',
+        'district': 'Hà Nội',
+        'routers': '10A, 54, 204'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.076359,21.188028]
+      },
+      'properties': {
+        'name': 'Thành phố Bắc Ninh',
+        'description': 'Gần Trung tâm văn hóa Kinh Bắc',
+        'address': 'Đường Kinh Dương Vương',
+        'district': 'Thành phố Bắc Ninh',
+        'routers': '54'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.088707,21.059260]
+      },
+      'properties': {
+        'name': 'Thuận Thành',
+        'description': 'Chợ trung tâm Thuận Thành',
+        'address': 'Đường Vương Văn Trà',
+        'district': 'Thị xã Thuận Thành',
+        'routers': '204'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.329320, 20.922313]
+      },
+      'properties': {
+        'name': 'Hải Dương',
+        'description': 'Bến xe Hải Tân',
+        'address': 'Đường Lê Thanh Nghị',
+        'district': 'Thành phố Hải Dương',
+        'routers': '217'
+      }
+    }
+  ]
+}
